97f3f2a1708d5826eeafb2f1f57daae65fd29554
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-11-10 15:22:08 -0300

    refactor: Took out capitalize() function in js code and change just the text-transform in the translated text's css

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const lastText = localStorage.getItem('last-translation');
 
-function format(text) {
-	function escapeNewEnglishWords(text) {
-		const regexp = /\{(.*?)\}/g;
-		return text.replaceAll(regexp, '');
-	}
-
-	function capitalize(text) {
-		return text[0].toUpperCase() + text.slice(1);
-	}
-
-	text = capitalize(escapeNewEnglishWords(text));
-	return text;
+function escapeNewEnglishWords(text) {
+	const regexp = /\{(.*?)\}/g;
+	return text.replaceAll(regexp, '');
 }
 
 function showTranslatedText(text) {
@@ -22,9 +13,9 @@ if (!lastText) {
 	fetch('https://api.funtranslations.com/translate/oldenglish.json?text=\'Slipping through my fingers all the time\'')
 		.then((res) => res.json())
 		.then((text) => {
-			showTranslatedText(format(text.contents.translated));
+			showTranslatedText(escapeNewEnglishWords(text.contents.translated));
 			localStorage.setItem('last-translation', text.contents.translated);
 		});
 } else {
-	showTranslatedText(format(lastText));
+	showTranslatedText(escapeNewEnglishWords(lastText));
 }
